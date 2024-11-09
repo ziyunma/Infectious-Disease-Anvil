720d7f61967ed614c52d3b37e8c915db68e546c0
cbowers4@nd.edu <cbowers4@nd.edu>
2024-11-09 19:21:12 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -8,3 +8,17 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def outlined_button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    input_file = self.text_box_1.text
+    sheetname = self.text_box_2.text
+    output_file = self.text_box_3.text
+    low_confidence_score = self.text_box_4.text
+    xmed_confidence_score = self.text_box_5.text
+    Notification('hey').show()
+    
+
+  def text_box_1_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    pass

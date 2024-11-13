ecd4af83def4ee8233716d7f80024c390124dacb
zma4@nd.edu <zma4@nd.edu>
2024-11-13 21:31:05 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -12,21 +12,37 @@ class Form1(Form1Template):
 
   def outlined_button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
-    input_file = self.text_box_1.text
-    sheetname = self.text_box_2.text
-    output_file = self.text_box_3.text
-    low_confidence_score = self.text_box_4.text
-    xmed_confidence_score = self.text_box_5.text
+    uploaded_file = self.file_loader_1.file  # This is the file uploaded through the FileLoader component
+        
+        # If there's no file uploaded, alert the user
+    if not uploaded_file:
+      alert("Please upload a file before submitting.")
+      return
+        
+        # Get the values from the text boxes
+      sheetname = self.text_box_2.text
+      output_file = self.text_box_3.text
+      low_confidence_score = float(self.text_box_4.text)  # Assuming these values are numeric
+      xmed_confidence_score = float(self.text_box_5.text)
 
-    anvil.server.call('standardize',
-                      input_file,
-                      sheetname,
-                      output_file,
-                      low_confidence_score,
-                      xmed_confidence_score
-                     )
+      # Call the server function and pass the file
+      result = anvil.server.call('standardize',
+                                  uploaded_file,  # Pass the uploaded file object
+                                  sheetname,
+                                  output_file,
+                                  low_confidence_score,
+                                  xmed_confidence_score
+                                )
+
+      # You can use `result` to display feedback to the user if needed
+      alert(result)
     
 
   def text_box_1_pressed_enter(self, **event_args):
     """This method is called when the user presses Enter in this text box"""
     pass
+
+  def file_loader_1_change(self, file, **event_args):
+    """This method is called when a new file is loaded into this FileLoader"""
+    if file:
+            self.text_box_1.text = file.name
